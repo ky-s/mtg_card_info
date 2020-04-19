4fd5ff833b368bedeeb3c34adf3bc99692677442
ky-s <kyohei_86@hotmail.com>
2020-04-19 15:41:25 +0900

    [update] update ruby 2.7

--- app/helpers/set_helper.rb
+++ app/helpers/set_helper.rb
@@ -6,13 +6,13 @@ module SetHelper
   end
 
   def fetch(set)
-    return link_to('Fetched', set_fetch_cards_path(set: set.code), remote: true, class: %w(btn btn-sm btn-secondary disabled m-2)) if set.fetched?
+    return link_to('Fetched', set_fetch_cards_path(set: set.code), remote: true, class: %w(btn btn-sm btn-secondary m-2)) if set.fetched?
     link_to('Fetch', set_fetch_cards_path(set: set.code), remote: true, class: %w(btn btn-sm btn-danger m-2 not_fetched))
   end
 
   def fetch_image(set)
     return link_to('Not feched', set_fetch_cards_image_path(set: set.code), remote: true, class: %w(btn btn-sm btn-danger disabled m-2)) unless set.fetched?
-    return link_to('Fetched', set_fetch_cards_image_path(set: set.code), remote: true, class: %w(btn btn-sm btn-secondary disabled m-2)) if set.image_fetched?
+    return link_to('Fetched', set_fetch_cards_image_path(set: set.code), remote: true, class: %w(btn btn-sm btn-secondary m-2)) if set.image_fetched?
     link_to('Fetch', set_fetch_cards_image_path(set: set.code), remote: true, class: %w(btn btn-sm btn-danger m-2 not_fetched))
   end
 end
--- app/services/mtg_downloader.rb
+++ app/services/mtg_downloader.rb
@@ -5,35 +5,42 @@ module MTGDownloader
     MTG::Set.all.sort_by(&:release_date).each do |set|
       mtg_set = MtgSet.find_or_initialize_by(code: set.code)
       mtg_set.update(
-        name: set.name,
-        type: set.type,
-        border: set.border,
-        mkm_id: set.mkm_id,
-        booster: set.booster,
+        name:         set.name,
+        type:         set.type,
+        border:       set.border,
+        mkm_id:       set.mkm_id,
+        booster:      set.booster,
         release_date: set.release_date,
-        block: set.block
+        block:        set.block
       )
     end
   end
 
   def fetch_card(set_code)
     cards = MTG::Card.where(set: set_code).all
-    cards.each.with_index(1) do |card, c|
+
+    cards.each.with_index(1) do |card, count|
       mtg_card = MtgCard.find_or_initialize_by(multiverse_id: card.multiverse_id)
       mtg_card.update(
-        name: card.name,
-        jp_name: card.jp_name,
-        img_url: card.img_url,
-        set: card.set,
-        rarity: card.rarity,
-        colors: card.colors&.join(','),
+        name:           card.name,
+        jp_name:        card.jp_name,
+        img_url:        card.img_url,
+        set:            card.set,
+        rarity:         card.rarity,
+        colors:         card.colors&.join(','),
         color_identity: card.color_identity&.join(','),
-        number: card.number,
-        artist: card.artist
+        number:         card.number,
+        artist:         card.artist
       )
 
-      ActionCable.server.broadcast('progress:1', percent: (c.to_f / cards.size.to_f * 100).to_i, set: set_code, action: 'fetch')
+      ActionCable.server.broadcast(
+        'progress:1',
+        percent: (count.to_f / cards.size.to_f * 100).to_i,
+        set:     set_code,
+        action:  'fetch'
+      )
     end
+
     MtgSet.find_by(code: set_code).update(fetched: true)
   end
 
@@ -45,24 +52,35 @@ module MTGDownloader
     Rails.logger.info "---------------------------------------------------------"
 
     cards = MtgCard.where(set: set_code)
-    cards.each.with_index(1) do |mtg_card, c|
+
+    cards.each.with_index(1) do |mtg_card, count|
       Rails.logger.info "loading and saving image #{mtg_card.jp_name}"
 
-      open(mtg_card.img_url) do |file|
-        uploaded_file = ActionDispatch::Http::UploadedFile.new(
-          filename: mtg_card.multiverse_id.to_s,
-          type: 'image/jpeg',
-          tempfile: file
-        )
-        mtg_card.image.attach(uploaded_file)
+      if !mtg_card.image.attached? && mtg_card.img_url.present?
+
+        open(mtg_card.img_url) do |file|
+          uploaded_file = ActionDispatch::Http::UploadedFile.new(
+            filename: mtg_card.multiverse_id.to_s,
+            type:     'image/jpeg',
+            tempfile: file
+          )
+          mtg_card.image.attach(uploaded_file)
+        end
+
       end
 
-      ActionCable.server.broadcast('progress:1', percent: (c.to_f / cards.size.to_f * 100).to_i, set: set_code, action: 'fetch_image')
+      ActionCable.server.broadcast(
+        'progress:1',
+        percent: (count.to_f / cards.size.to_f * 100).to_i,
+        set:     set_code,
+        action:  'fetch_image'
+      )
 
       Rails.logger.info "completed image #{mtg_card.jp_name}"
     end
 
     MtgSet.find_by(code: set_code).update(image_fetched: true)
+
     Rails.logger.info "---------------------------------------------------------"
     Rails.logger.info "mtg:card:fetch_image[#{set_code}] done."
     Rails.logger.info "---------------------------------------------------------"
